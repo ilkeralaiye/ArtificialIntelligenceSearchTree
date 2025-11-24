5455e2205ead64e582ca260ab5eeba04055f2a79
ilkeralaiye <ilkeralaiye@hotmail.com>
2025-11-24 18:35:34 +0300

    feat: implement Tree, TreeNode, and subTreeNode classes

---
+++ ArtificialIntelligenceSearchTree/MainTreeNode.cs
@@ -0,0 +1,17 @@
+﻿namespace ArtificialIntelligenceSearchTree {
+    internal class MainTreeNode {
+
+        public MainTreeNode left;
+        public MainTreeNode right;
+
+        public Tree subTree;
+
+        public string name;
+
+        public MainTreeNode(string name) {
+            subTree = new Tree();
+            this.name = name;
+        }
+
+    }
+}
---
+++ ArtificialIntelligenceSearchTree/SubTreeNode.cs
@@ -0,0 +1,19 @@
+﻿namespace ArtificialIntelligenceSearchTree {
+    internal class SubTreeNode {
+
+        public SubTreeNode left;
+        public SubTreeNode right;
+
+        public string name;
+
+        public string description;
+        public string applicationAreas;
+
+        public SubTreeNode(string name, string description, string applicationAreas) {
+            this.name = name;
+            this.description = description;
+            this.applicationAreas = applicationAreas;
+        }
+
+    }
+}
